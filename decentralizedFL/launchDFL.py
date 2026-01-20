51630b8da96762c584ca428b893a55f55bfb6554
anbettoli <antonin.bettoli@etu.uca.fr>
2026-01-20 15:03:38 +0100

    first implementation of decentralized learning

---
+++ decentralizedFL/launchDFL.py
@@ -0,0 +1,49 @@
+import os
+import sys
+
+
+module_path = os.path.abspath(os.path.join('..'))
+sys.path.insert(0, module_path)
+
+from fluke.data.datasets import Datasets
+dataset = Datasets.get("mnist", path="./data")
+
+from fluke.data import DataSplitter
+splitter = DataSplitter(dataset=dataset,
+                        distribution="iid")
+
+from fluke.evaluation import ClassificationEval
+from fluke import FlukeENV
+
+evaluator = ClassificationEval(eval_every=1, n_classes=dataset.num_classes)
+FlukeENV().set_evaluator(evaluator)
+
+from fluke import DDict
+client_hp = DDict(
+    batch_size=10,
+    local_epochs=5,
+    loss="CrossEntropyLoss",
+    optimizer=DDict(
+      name="SGD",
+      lr=0.01,
+      momentum=0.9,
+      weight_decay=0.0001),
+    scheduler=DDict(
+      gamma=1,
+      step_size=1)
+)
+
+# we put together the hyperparameters for the algorithm
+hyperparams = DDict(client=client_hp,
+                    server=DDict(weighted=True),
+                    model="MNIST_2NN")
+
+# import your own algorithm here
+from p2p import P2P
+algorithm = P2P(7, splitter, hyperparams)
+
+from fluke.utils.log import Log
+logger = Log()
+algorithm.set_callbacks(logger)
+
+algorithm.run(10, 0.5)
--- decentralizedFL/p2p.py
+++ decentralizedFL/p2p.py
@@ -1,19 +1,108 @@
-from typing import Collection, Sequence
-from torch.nn import Module
-from fluke.client import Client
-from fluke.server import Server
+import copy
 import numpy as np
+import torch
+from fluke.server import Server
+from fluke.client import Client
+from fluke.comm import Message
+from fluke.utils.model import aggregate_models
+
+
 class P2PServer(Server):
+    def __init__(self, model, test_set, clients, **kwargs):
+        """
+        adjacency_matrix: dict[int, list[int]] 
+                          Ex: {0: [1, 2], 1: [0, 3], ...}
+        """
+        super().__init__(model, test_set, clients, **kwargs)
 
-    # we override the aggregate method to implement our aggregation strategy
-    def aggregate(self, eligible: Sequence[Client], client_models: Collection[Module]) -> None:
+        # modifier la matrice d'adjacence ici
+        self.adjacency_matrix = {0: [1,2], 1: [2], 2: [1,3,5], 3: [2], 4: [6], 5: [2,6], 6: [5,4]}
+        
+        # Initialisation : On clone le state_dict initial pour chaque client
+        initial_state = model.state_dict()
+        self.client_states = {
+            client.index: copy.deepcopy(model.state_dict()) # <--- CORRECT : On extrait les poids
+            for client in clients
+        }
 
-        # eligible is a list of clients that participated in the last round
-        # here we randomly select only two of them
-        selected = np.random.choice(eligible, 2, replace=False)
+    def broadcast_model(self, eligible):
+        """
+        Surcharge de la méthode de server.py.
+        Au lieu de broadcaster le self.model global, on envoie à chaque client
+        sa version spécifique stockée dans self.client_states.
+        """
+        for client in eligible:
+            # 1. Charger l'état spécifique du client dans le conteneur du serveur
+            my_state = self.client_states[client.index]
+            self.model.load_state_dict(my_state)
+            
+            # 2. Créer une copie pour l'envoi (important pour éviter les conflits mémoire en simulation)
+            # On envoie un objet modèle car client.py attend un objet ayant .state_dict()
+            model_to_send = copy.deepcopy(self.model)
+            
+            # 3. Envoyer en unicast via le channel
+            # self.channel.send(
+            #     Message(model_to_send, "model", dst=client.index, src="server"), 
+            #     dest=str(client.index) # Le channel attend souvent un str ou l'ID direct selon l'implémentation
+            # )
+            self.channel.broadcast(
+                Message(model_to_send, "model", "server"),
+                [client.index]
+            )
 
-        # we call the parent class method to aggregate the selected clients
-        return super().aggregate(selected, client_models)
+    def aggregate(self, eligible, client_models_gen):
+        """
+        Logique de Gossip / Consensus.
+        1. On récupère les mises à jour des clients qui ont travaillé.
+        2. Pour chaque client, on agrège son nouveau modèle avec ceux de ses voisins.
+        """
+
+        # conversion du générateur en liste si nécessaire
+        client_models = list(client_models_gen)
+
+        # 1. Mise à jour des états LOCAUX
+        # ATTENTION : server.fit() envoie des OBJETS modèles, il faut extraire le state_dict
+        for client, model_obj in zip(eligible, client_models):
+            # C'est ici qu'on évite l'erreur pour le prochain round
+            self.client_states[client.index] = copy.deepcopy(model_obj.state_dict())
+
+        # 2. Logique de mélange (Gossip)
+        next_round_states = {}
+        
+        for client in self.clients:
+            cid = client.index
+            neighbors = self.adjacency_matrix.get(cid, [])
+            
+            # On récupère les dictionnaires (state_dicts) stockés
+            states_to_avg = [self.client_states[cid]]
+            for nid in neighbors:
+                if nid in self.client_states:
+                    states_to_avg.append(self.client_states[nid])
+            
+            # On fait la moyenne des dictionnaires
+            next_round_states[cid] = self._average_state_dicts(states_to_avg)
+
+        self.client_states = next_round_states
+        
+        # Optionnel : On met à jour self.model avec la moyenne globale juste pour l'évaluation "Server-side"
+        # si vous voulez garder une trace de la performance globale du réseau.
+        all_states = list(self.client_states.values())
+        #self.model.load_state_dict(self._average_state_dicts(all_states))
+
+    def _average_state_dicts(self, state_dicts):
+        """Fonction utilitaire pour moyenner une liste de state_dicts."""
+        if not state_dicts:
+            return None
+        
+        avg_state = copy.deepcopy(state_dicts[0])
+        n = len(state_dicts)
+        
+        for key in avg_state.keys():
+            # On gère les tenseurs float, on ignore les LongTensor (ex: num_batches_tracked) souvent non moyennables
+            if avg_state[key].is_floating_point():
+                avg_state[key] = sum(d[key] for d in state_dicts) / n
+                
+        return avg_state
     
 class P2PClient(Client):
 
